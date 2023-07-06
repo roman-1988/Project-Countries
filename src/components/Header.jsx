ef5387bb6a1560833beeab940dc7cd913912e5b9
Roman <roman-1@inbox.ru>
2023-07-06 11:01:56 +0300

    Добавил стилей в стилизованные компоненты. С помощью хуков и обработчика настроил темизацию светлой и темной темы. Поменял у светлой темы иконку на солнце.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,46 @@
 import styled from "styled-components"
+import { useState, useEffect } from "react"
 import { Container } from "./Container"
-import { IoMoon, IoMoonOutline } from "react-icons/io5"
+import { IoMoon, IoSunnyOutline } from "react-icons/io5"
 
-const HeaderEl = styled.header``
+const HeaderEl = styled.header`
+ box-shadow: var(--shadow);
+ background-color: (--colors-ui-base);
+`
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ padding: 2rem 0;
+`
 
 const Title = styled.a.attrs({
     href: "/"
-})``
+})`
+ color: var(--colors-text);
+ font-size: var(--fs-sm);
+ text-decoration: none;
+ font-weight: var(--fw-bold)
+`
 
-const ModeSwitcher = styled.div``
+const ModeSwitcher = styled.div`
+color: var(--colors-text);
+font-size: var(--fs-sm);
+font-weight: var(--fw-bold);
+text-transform: capitalize;
+cursor: pointer;
+`
 
 const Header = () => {
+    const [theme, setTheme] = useState("light")
+
+    const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light")
+
+    useEffect(() => {
+        document.body.setAttribute("data-theme", theme)
+    }, [theme])
+
     return (
         <HeaderEl>
             <Container>
@@ -20,8 +48,9 @@ const Header = () => {
                     <Title>
                         Where is the world?
                     </Title>
-                    <ModeSwitcher>
-                        <IoMoon /> Light Theme
+                    <ModeSwitcher onClick={toggleTheme}>
+                        {theme === "light" ? (<IoSunnyOutline size="14px" />) : (<IoMoon size="14px" />)}
+                        {" "} <span style={{ marginLeft: "0.75rem" }}>{theme} Theme</span>
                     </ModeSwitcher>
                 </Wrapper>
             </Container>
